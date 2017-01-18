26a4bd4e776b1c2979fccab16f6f35a745ad6002
Carey Costa <careyxcosta@gmail.com>
2017-01-18 16:15:30 -0500

    exercise3 updated

--- exercise3.rb
+++ exercise3.rb
@@ -9,27 +9,31 @@ class Player
     @score = 0
   end
 
-    def do_battle
-      health_points - 1
-      lives - 1 if health_points == 0
-      health_points == 10 if health_points == 0
-    end
+  def restart
+    gold_coins == 0
+    health_points == 10
+    lives == 5
+    score == 0
+  end
 
-    def restart
-      gold_coins == 0
-      health_points == 10
-      lives == 5
-      score == 0
+    def do_battle
+      @health_points -= 1
+      if @health_points <= 0
+        @lives -= 1
+        @health_points = 10
     end
+    restart if @lives <= 0
+  end
 
     def level_up
       lives + 1
     end
 
-    def collect_treasure(num)
-      num + gold_coins
-      score + 1 if gold_coins == 10
-      if score == 10
+    def collect_treasure(treasure)
+      @gold_coins += treasure
+      if gold_coins >= 10
+        @score += 1
+        @gold_coins == 0
     end
 
 end
